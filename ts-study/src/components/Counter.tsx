4fd44da9ae1d69dc3c2506746d9bdf7ba7a9ec2b
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-25 08:43:42 +0900

    counter

--- ts-study/src/components/Counter.tsx
+++ ts-study/src/components/Counter.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useReducer, useState } from "react";
 
-interface IProps {
-  count: number;
-  onIncrease: () => void;
-  onDecrease: () => void;
-  onIncreaseBy: (diff: number) => void;
-};
+type Action = { type: "INCREASE" } | { type: "DECREASE" };
 
-function Counter({
-  count,
-  onIncrease,
-  onDecrease,
-  onIncreaseBy
-}: IProps) {
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      throw new Error("action");
+  }
+}
+
+function Counter() {
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREASE" });
+  const onDecrease = () => dispatch({ type: "DECREASE" });
+
   return (
-    <div>
+    <>
       <h1>{count}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
-      <button onClick={() => onIncreaseBy(5)}>+5</button>
-    </div>
+    </>
   );
 }
 
