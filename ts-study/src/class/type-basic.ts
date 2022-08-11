7351c58d7061ea1f69c6ba2a6c76c13d5428caee
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-08-12 08:46:11 +0900

    basic

---
+++ ts-study/src/class/type-basic.ts
@@ -0,0 +1,23 @@
+// TS 변수 선언
+// string
+let str: string = 'hello';
+// number
+let num: number = 3;
+// array
+let arr: Array<number> = [1, 2, 3];
+let names: Array<string> = ['song', 'chang', 'seok'];
+let items: number[] = [4, 5, 6];
+// 튜플 : 모든 index에 타입이 있다.
+let address: [string, number] = ['seoul', 100];
+// object
+let obj: object = {};
+// let person: object = {
+//   name: 'song',
+//   age: 29
+// };
+let person: {name: string, age: number} = {
+  name: 'song',
+  age: 29
+};
+// 진위값
+let show: boolean = true;
