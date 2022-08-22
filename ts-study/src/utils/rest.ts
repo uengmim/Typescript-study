858e73761d07c09d29dca885e4880b097e776f5c
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-08-23 08:36:24 +0900

    rest

---
+++ ts-study/src/utils/rest.ts
@@ -0,0 +1,10 @@
+
+let address: any = {
+    country: 'Korea',
+    city: 'Seoul',
+    address1: 'Gangnam-gu',
+    address2: 'Sinsa-dong',
+    address3: '123-456',
+  };
+  
+  export const { country, city, ...detail} = address;
