f6599a34d2fb6cbeefc217687191313bbc4bb0f9
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-12-02 08:45:30 +0900

    sum

--- ts-study/src/iterator/declarative-odd-sum.ts
+++ ts-study/src/iterator/declarative-odd-sum.ts
@@ -5,6 +5,4 @@ import { filter } from './filter';
 let numbers: number[] = range(1, 100 + 1);
 
 const isOdd = (n: number): boolean => n % 2 !== 0;
-let result = fold(filter(numbers, isOdd), (result, value) => result + value, 0);
 
-console.log(result);
